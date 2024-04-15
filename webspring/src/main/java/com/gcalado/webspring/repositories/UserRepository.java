753ee2476432f1201d66c05cf5cfa614e4cc19e6
Gabriel Calado <gcalado501@gmail.com>
2024-04-15 17:15:05 -0300

    Service layer, component registration

--- webspring/src/main/java/com/gcalado/webspring/repositories/UserRepository.java
+++ webspring/src/main/java/com/gcalado/webspring/repositories/UserRepository.java
@@ -3,7 +3,6 @@ package com.gcalado.webspring.repositories;
 import com.gcalado.webspring.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
