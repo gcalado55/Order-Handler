24f43a1cce2335790786ce9886c0dbdbd773223b
Gabriel Calado <gcalado501@gmail.com>
2024-05-03 17:30:15 -0300

    Exception handling - update

--- webspring/src/main/java/com/gcalado/webspring/services/UserService.java
+++ webspring/src/main/java/com/gcalado/webspring/services/UserService.java
@@ -4,6 +4,7 @@ import com.gcalado.webspring.entities.User;
 import com.gcalado.webspring.repositories.UserRepository;
 import com.gcalado.webspring.services.exceptions.DatabaseException;
 import com.gcalado.webspring.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -44,9 +45,14 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }
+        catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
